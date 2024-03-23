079cced03b82864894d476d81314be9f409e6669
Toonpb <164642467+Toonpb@users.noreply.github.com>
2024-03-23 14:36:22 +0100

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":train:", layout="wide")
+
+st.title("Fever Dream")
+st.subheader("Haagse band")
+
+  
+
